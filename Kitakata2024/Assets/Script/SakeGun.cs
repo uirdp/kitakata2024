736991164f26e5f2e19104398bfd754f2facd130
uirdp <haumuchan@gmail.com>
2024-05-06 16:37:21 +0900

    object pool input bug

--- Kitakata2024/Assets/Script/SakeGun.cs
+++ Kitakata2024/Assets/Script/SakeGun.cs
@@ -1,12 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Pool;
 
 namespace SakeShooter
 {
     public class SakeGun : MonoBehaviour
     {
+        public GameObject bulletPrefab;
+
+        public bool collectionChecks = true;
+        public int maxPoolSize = 30;
+        
+        private IObjectPool<GameObject> _bulletPool;
+        
 #if ENABLE_INPUT_SYSTEM
         private PlayerInput _playerInput;
 #endif
@@ -20,6 +29,9 @@ namespace SakeShooter
 #else
 			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
 #endif
+            
+            _bulletPool = new ObjectPool<GameObject>(CreateBullet, OnTakeFromPool, OnReturneToPool, OnDestroyPoolObject,
+                                                     collectionChecks, 10, maxPoolSize);
         }
         
         private void Update()
@@ -32,7 +44,35 @@ namespace SakeShooter
             if (_input.fire)
             {
                 Debug.Log("Fire");
+
+                var bullet = _bulletPool.Get();
             }
         }
+
+        #region ObjectPool
+            
+            private GameObject CreateBullet()
+            {
+                var bullet = Instantiate(bulletPrefab);
+                bullet.SetActive(false);
+                return bullet;
+            }
+            
+            private void OnTakeFromPool(GameObject bullet)
+            {
+                bullet.SetActive(true);
+            }
+            
+            private void OnReturneToPool(GameObject bullet)
+            {
+                bullet.SetActive(false);
+            }
+
+            private void OnDestroyPoolObject(GameObject bullet)
+            {
+                Destroy(bullet);
+            }
+            
+            #endregion
     }
 }
