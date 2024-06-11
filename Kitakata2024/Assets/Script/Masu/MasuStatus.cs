46979e6bebf1cb8ef7f20980e0ad036824d5c9db
uirdp <haumuchan@gmail.com>
2024-06-11 11:18:51 +0900

    いい感じです。　ます。

--- Kitakata2024/Assets/Script/Masu/MasuStatus.cs
+++ Kitakata2024/Assets/Script/Masu/MasuStatus.cs
@@ -8,19 +8,36 @@ public class MasuStatus : MonoBehaviour
     public float capacity = 100.0f;
     private float _currentAmount;
     
-    private event Action<float> OnFill; 
+    private event Action<float> OnFill;
+    private event Action OnFullyFilled;
     private void Initialize()
     {
         _currentAmount = 0f;
         OnFill += Fill;
+        OnFullyFilled += FullEvent;
     }
     
     private void Fill(float amount)
     {
         _currentAmount += amount;
-        if (_currentAmount > capacity)
+        Debug.Log(amount);
+    }
+
+    private void FullEvent()
+    {
+        Debug.Log("Full!");
+    }
+
+    //あとはこいつをColliderに登録するだけ！！！
+    private void OnHitDetected()
+    {
+        if(_currentAmount < capacity)
         {
-            _currentAmount = capacity;
+            OnFill?.Invoke(10.0f);
+        }
+        else
+        {
+            OnFullyFilled?.Invoke();
         }
     }
 
